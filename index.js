2bd5e75befbfc0e695f4612ace8777f5b0c25fd8
Noaman-Saleem <noaman.saleem@umt.edu.pk>
2021-06-24 16:45:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 
-const Blog=require('./models/blogs.js')
+const Blog = require('./models/blogs.js')
+//connected to mongoDB
+mongoose.connect('mongodb://localhost:27017/blog-app', { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connected to MongoDB")
+	})
+	.catch(e => {
+		console.log('OH NON ERROR')
+		console.log(e)
+	})
 
-mongoose.connect('mongodb://localhost:27017/blog-app', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{
-	console.log("Connected to MongoDB")
-})
-.catch(e=>{
-	console.log('OH NON ERROR')
-	console.log(e)
-})
+app.set('view engine', 'ejs')
 
-app.set('view engine','ejs')
-
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
 
-app.get('/',async (req,res)=>{
+app.get('/', async (req, res) => {
 	const allBlogs = await Blog.find({})
-	res.render('home',{allBlogs})
+	res.render('home', { allBlogs })
 })
 
-app.get('/blogs/new',(req,res)=>{
+app.get('/blogs/new', (req, res) => {
 	res.render('newBlog')
 })
 
-app.post('/blogs', async (req,res)=>{
+app.post('/blogs', async (req, res) => {
 	// console.log(req.body)
 	const blog = await new Blog(req.body)
 	blog.save()
 	res.redirect('/')
 })
 
-app.get('/blogs/:id/edit',async (req,res)=>{
-	const {id}=req.params
+app.get('/blogs/:id/edit', async (req, res) => {
+	const { id } = req.params
 	const blog = await Blog.findById(id)
-	res.render('editBlog',{blog})
+	res.render('editBlog', { blog })
 })
 
-app.put('/blogs/:id',async (req,res)=>{
-	const {id}=req.params
-	const blog =await Blog.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
+app.put('/blogs/:id', async (req, res) => {
+	const { id } = req.params
+	const blog = await Blog.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
 	res.redirect('/')
 })
 
@@ -54,12 +54,12 @@ app.put('/blogs/:id',async (req,res)=>{
 // 	res.redirect('/')
 // })
 
-app.delete('/blogs/:id',async (req,res)=>{
-	const {id}=req.params
+app.delete('/blogs/:id', async (req, res) => {
+	const { id } = req.params
 	await Blog.findByIdAndDelete(id)
 	res.redirect('/')
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
 	console.log('Server started at PORT 3000')
 })
